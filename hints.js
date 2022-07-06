95c5ec7a5594f44e3ade8082365f10ae71a5e3bc
Paul <92057791+Pauyy@users.noreply.github.com>
2022-07-06 20:37:15 +0200

    Added a new hint, code "1.0.0"

--- hints.js
+++ hints.js
@@ -7,7 +7,11 @@ function loadHintsByCode(code){
 					"Hints are an optional part of gameplay.<br> These are sometimes helpful and sometimes they refuse to help.<br>This Website tries to remake the hint page. If you compare it to the original, I'd say it looks not that bad :)",
 					"The main character is a professor of archaeology, but you rarely see him doing his profession."];
 		break;
-
+	case "1.0.0":
+		hintText = ["The perk in question is a killer perk.", 
+					"The perk is not bound to a specific killer, it is a general perk.",
+					"Actually the perk is about to get reworked in Patch 6.1.0"];
+		break;
 	default:
 		correct = false;
 		break;
